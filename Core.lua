8883f4a66b5d0cf7a6091dea6e89d719d11b94ab
sinistercashew <99692889+sinistercashew@users.noreply.github.com>
2022-02-14 15:59:50 -0600

    Added Files Via Upload
    Initial Commit

---
+++ Core.lua
@@ -0,0 +1,13 @@
+BigWigsAPI:RegisterCountdown("Zuji Nerd", {
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_1.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_2.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_3.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_4.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_5.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_6.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_7.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_8.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_9.ogg",
+	"Interface\\AddOns\\BigWigs_CountDown_Sigma\\Media\\Sounds\\Zuji\\Nerd\\Nerd_10.ogg"
+})
+
